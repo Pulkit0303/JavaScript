236f7ea2611fc6ffc2b4bc40ab674ed9c497d9b7
Pulkit1903 <pulkit.19.2003@gmail.com>
2025-07-23 16:01:27 +0530

    DataTypes is explained.

---
+++ 02_datatypes.js
@@ -0,0 +1,30 @@
+"use strict"; //Used to indicate the newer version of JS is used
+//By default it is used
+
+//alert(3+3) //We should not be using this because we are not using browser to run our code.
+
+
+//Never Ever type these kind of code in your application
+
+/*console.log(3+
+    3);
+    console.log("hi"); console.log("bye")
+*/
+
+let name = "Pulkit"
+let age = 22
+let isLogged = false
+
+/*
+1. number => 2 power 53(Max)
+2. bigint       //For large values (Facebook, Stocks)
+3. string => " "
+4. boolean => true/false
+5. null         //This is a standalone value
+6. undefined 
+
+*/
+
+console.table([typeof age, typeof "Pulkit", typeof undefined]);
+
+console.log(typeof null);   //Very Much Impt:   null is a type of an object
