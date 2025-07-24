41c24d713a185f8230cd11ecd1e4d8b13f3e0177
Pulkit1903 <pulkit.19.2003@gmail.com>
2025-07-24 12:57:16 +0530

    Learned about Prefix and Postfix Increment

--- 03_conversion.js
+++ 03_conversion.js
@@ -28,10 +28,53 @@ let booleanIsLoggedIn = Boolean(isLoggedIn);
 
 let someNumber = true;
 let stringNumber = String(someNumber);
-console.log(typeof(stringNumber));
-console.log(stringNumber);
+// console.log(typeof(stringNumber));
+// console.log(stringNumber);
 
 // 33 => "33"
 // null => "null"
 // undefined => "undefined"
 // true => "true" ; false => "false"
+
+
+// ************************Operations*************************
+let valueNum = 9
+let negValue = -valueNum
+//console.log(negValue);
+
+
+// console.log(2+3)
+// console.log(2-3)
+// console.log(2*3);
+// console.log(2**3);
+// console.log(3%2);
+
+// console.log("1" +2);
+// console.log("2"+2+8);
+// console.log(1+1+"3");
+
+
+// console.log(2+3*4/4); NEVER WRITE THIS TYPE OF OPERATIONS
+//console.log((2+3) * (4/4)); // ALWAYS Use () for seperation of operations
+
+//console.log(+true);
+
+//Since the value of True is 1
+//console.log(true+9);
+
+/*  Preffix and Postfix Increment
+let n1 = 0;
+console.log(n1++); // The value is first printed then Incremented.
+console.log(++n1);  // The value is first incremented and then printed.
+console.log(n1); // The updated value is printed.
+*/
+
+
+
+
+
+
+
+
+
+
