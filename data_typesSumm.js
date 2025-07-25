413509e5a601ed1f2d00a9ff62be17e90e0e2622
Pulkit1903 <pulkit.19.2003@gmail.com>
2025-07-25 15:26:37 +0530

    Learned about Heap and Stack Memory

--- data_typesSumm.js
+++ data_typesSumm.js
@@ -33,7 +33,33 @@ const myFunction = function(){
 //Calling a function
 myFunction()
 
-console.log();
+console.log(typeof myFunction);
 
+//******************Memory Types*******************
+//1. Stack(Primitive)  : (Giving photocopy of something)
 
+let myvalue = "pulkit.com"
+let anothervalue = myvalue
+anothervalue = "pulkit.in"
+
+console.log(myvalue);
+console.log(anothervalue);
+
+// 2. Heap(Non-Prmitive) : (Giving original photo)
+
+let driver = {
+    name: "Lewis",
+    team: "Mercedes AMG Petronas"
+}
+
+let driver2 = driver
+driver2.name = "Russell"
+
+/*When you copy an object in JavaScript:
+You’re not making a new object.
+You’re just giving a new name (variable) to the same object in memory.
+Both driver and driver2 are just labels for the same object. So when driver2 changes it, driver also sees the change.
+*/
+console.log(driver.name);
+console.log(driver2.name);
 
