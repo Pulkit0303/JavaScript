7c9a24dc06458f55f5264fa438ab7db8a5e1f22c
Pulkit1903 <pulkit.19.2003@gmail.com>
2025-08-03 11:09:25 +0530

    Objects re-factoring

--- Arrays-to-_/04_object.js
+++ Arrays-to-_/04_object.js
@@ -17,8 +17,8 @@ const User = {
         }
     }
 }
-// console.log(User);
-// console.log(User.fullname.userfullname.firstname); // To acces element from multi-dimensional object
+console.log(User);
+console.log(User.fullname.userfullname.firstname); // To acces element from multi-dimensional object
 
 //To join two objects
 const obj1 = {1:"a",2:"b"}
@@ -29,8 +29,8 @@ const obj2 = {3:"a",4:"b"}
 // The reason we are providing an {<emptyObject>} so that values of obj1 does not get affected.
 
 const combined = {...obj1,...obj2} // This type of combining will be used most of the time
-// console.log(combined)
-// console.log(obj1)
+console.log(combined)
+console.log(obj1)
 
 // When we get data from a database it is in the form of Object inside an array
 const dbmessage = [{
@@ -43,15 +43,15 @@ const dbmessage = [{
     name: "Raj",
     age: 23
 }]
-//console.log(dbmessage[1].name) // This is how we can access elements from the Object in an array
+console.log(dbmessage[1].name) // This is how we can access elements from the Object in an array
 
-//console.log(tinderUser);
-// console.log(Object.keys(tinderUser));// To get the keys in an Object
-// console.log(Object.values(tinderUser)); // To get the values of an Object
-// console.log(Object.entries(tinderUser)); // To get entries in an object (Content inside an object)
+console.log(tinderUser);
+console.log(Object.keys(tinderUser));// To get the keys in an Object
+console.log(Object.values(tinderUser)); // To get the values of an Object
+console.log(Object.entries(tinderUser)); // To get entries in an object (Content inside an object)
 
-// console.log(tinderUser.hasOwnProperty(["name"])) // To check whether the Object has an field of <name>
-// console.log(tinderUser.hasOwnProperty(["address"])) // Returns a boolean value
+console.log(tinderUser.hasOwnProperty(["name"])) // To check whether the Object has an field of <name>
+console.log(tinderUser.hasOwnProperty(["address"])) // Returns a boolean value
 
 /*********Object De-Structure************/
 const course = {
