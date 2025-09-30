ba88e344a76a865c8fd544c05452c17ef3d6b9cd
Pulkit1903 <pulkit.19.2003@gmail.com>
2025-09-30 18:20:49 +0530

    Updated

---
+++ Variables-to-Date&Time/tempCodeRunnerFile.js
@@ -0,0 +1,6 @@
+const date1 = new Date("2003-03-19 05:15:30")
+// console.log(date1.toDateString());
+// console.log(date1.getDay()); // To get the day of the week (0-6, where 0 is Sunday)
+// console.log(date1.getMonth()+1); // To get the month (0-11, where 0 is January)
+// console.log(date1.getFullYear()); // To get the full year
+
